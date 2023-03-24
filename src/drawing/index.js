d64f0f6eed1cc5ee38ba9fbc52bba154d6aa1ff6
Stanko <stanko.tadic@gmail.com>
2023-03-24 10:22:52 +0100

    WIP

--- src/drawing/index.js
+++ src/drawing/index.js
@@ -1,12 +1,13 @@
 import p5constructor from 'p5';
 import setMainSeed from '../utils/set-main-seed';
-import { generateConvexPolygon } from '../../../generative-utils/polygons'
+import { generateConvexPolygon } from '../../../generative-utils/polygons';
 import { getPointOnLine } from '../../../generative-utils/points';
 import { getVectorAngle } from '../../../generative-utils/vectors';
-import Particle from './particle';
+import Particle, { SCALE } from './particle';
 import seedrandom from 'seedrandom';
 import random from '../utils/random';
 import { saveAs } from 'file-saver';
+import * as clipperLib from 'js-angusj-clipper';
 
 let sketchInstance;
 
@@ -19,19 +20,29 @@ const svgDownloadButton = document.querySelector('.svg-download');
 
 svgDownloadButton.addEventListener('click', () => {
   const name =
-  'swirl-' + window.location.hash.replace("#/", "").replace(/\//g, "-") + ".svg";
-  saveAs(`data:application/octet-stream;base64,${btoa(svgElement.outerHTML)}`, name);
+    'swirl-' +
+    window.location.hash.replace('#/', '').replace(/\//g, '-') +
+    '.svg';
+  saveAs(
+    `data:application/octet-stream;base64,${btoa(svgElement.outerHTML)}`,
+    name
+  );
 });
 
 let polygon;
 
 function getColor(rng, startColor, colorRange) {
-  return `hsl(${ random(startColor, startColor + colorRange, rng, 0) }, ${ random(60, 90, rng, 0) }%, ${ random(30, 80, rng, 0) }%)`;
+  return `hsl(${random(startColor, startColor + colorRange, rng, 0)}, ${random(
+    60,
+    90,
+    rng,
+    0
+  )}%, ${random(30, 80, rng, 0)}%)`;
 }
 
 function prepare(options) {
-  const center = { 
-    x: options.width / 2, 
+  const center = {
+    x: options.width / 2,
     y: options.height / 2,
   };
   const particles = [];
@@ -52,40 +63,62 @@ function prepare(options) {
       const directionMap = {
         circular: p5.random() > 0.5 ? -1 : 1,
         interlaced: p5.random() > 0.5 ? -2 : 2,
-      } 
+      };
       const direction = directionMap[options.direction];
 
-      const angle = getVectorAngle({
-        x: p2.x - p.x,
-        y: p2.y - p.y,
-      }) + Math.PI / direction + Math.PI * p5.random(-0.2, 0.2);
+      const angle =
+        getVectorAngle({
+          x: p2.x - p.x,
+          y: p2.y - p.y,
+        }) +
+        Math.PI / direction +
+        Math.PI * p5.random(-0.2, 0.2);
 
-      const color = options.grayscale ? 
-        Math.round(p5.random(30, 240)) :
-        getColor(colorRNG, startColor, options.colorRange);
+      const color = options.grayscale
+        ? Math.round(p5.random(30, 240))
+        : getColor(colorRNG, startColor, options.colorRange);
 
-      particles.push(new Particle({
-        position,
-        angle,
-        color,
-        length: random(options.minLength, options.maxLength, null, 0),
-        size: random(options.minStartingSize, options.maxStartingSize),
-        speed: random(2, 3),
-      }));
+      particles.push(
+        new Particle({
+          position,
+          angle,
+          color,
+          length: random(options.minLength, options.maxLength, null, 0),
+          size: random(options.minStartingSize, options.maxStartingSize),
+          speed: random(2, 3),
+        })
+      );
     }
   });
-  
+
   return p5.shuffle(particles);
 }
 
-export default function drawing(options) {
-  const {
-    width,
-    height,
-    mainSeed,
-    renderSvg,
-  } = options;
-  
+function getSvgPolygon(polygon, color) {
+  const paths = polygon.map((part) => {
+    const l = part
+      .map((point) => {
+        return `${point.x / SCALE} ${point.y / SCALE}`;
+      })
+      .join(' L ');
+
+    return `<path d="${`M ${l} Z`}" fill="none"  stroke="${color}" />`;
+  });
+
+  return paths.join('\n');
+}
+
+export default async function drawing(options) {
+  if (!window.clipper) {
+    // create an instance of the library (usually only do this once in your app)
+    window.clipper = await clipperLib.loadNativeClipperLibInstanceAsync(
+      // let it autodetect which one to use, but also available WasmOnly and AsmJsOnly
+      clipperLib.NativeClipperLibRequestedFormat.WasmWithAsmJsFallback
+    );
+  }
+
+  const { width, height, mainSeed, renderSvg } = options;
+
   // Swap Math.random for a seeded rng
   setMainSeed(mainSeed);
 
@@ -115,27 +148,34 @@ export default function drawing(options) {
 
     p5.setup = () => {
       p5.createCanvas(width, height);
-  
+
       particles = prepare(options);
     };
-  
+
     p5.draw = () => {
       p5.noFill();
       p5.noStroke();
 
       if (p5.frameCount > options.maxLength) {
-        particles.forEach(p => {
-          svgContent += p.draw(true);
+        svgContent = '';
+        let circles = '';
+
+        particles.forEach((p) => {
+          circles += p.draw(true);
+          svgContent += getSvgPolygon(p.polygon, p.color);
         });
-        
+
+        svgContent += circles;
+
         p5.noLoop();
 
         if (renderSvg) {
           svgElement.innerHTML = svgContent;
         }
       } else {
-        particles.forEach(p => {
-          svgContent += p.draw();
+        particles.forEach((p) => {
+          p.draw();
+          // svgContent += p.draw();
           p.move();
         });
       }
--- src/drawing/particle.js
+++ src/drawing/particle.js
@@ -1,6 +1,12 @@
+import simplify from 'simplify-js';
+import random from '../utils/random';
+
+export const SCALE = 1000;
+const SIMPLIFY_TOLERANCE = 0.35;
+
 class Particle {
   constructor({
-    position, 
+    position,
     angle = Math.PI * 0.3,
     color = '#667788',
     length = 200,
@@ -21,22 +27,77 @@ class Particle {
     this.color = color;
     this.originalColor = color;
 
-    
+    this.polygon = [];
+    this.circles = [];
+
     if (typeof this.color === 'number') {
-      this.color = `#${ this.color.toString(16) }${ this.color.toString(16) }${ this.color.toString(16) }`;
+      this.color = `#${this.color.toString(16)}${this.color.toString(
+        16
+      )}${this.color.toString(16)}`;
     }
+
+    const colors = ['#f1c40f', '#3498db', '#e74c3c'];
+
+    this.color = colors[random(0, colors.length - 1, null, 0)];
 
     this.sizeStep = p5.random(0.2, 0.4);
 
     this.done = false;
   }
-  
+
+  getCircle() {
+    const x = this.position.x;
+    const y = this.position.y;
+    const r = this.size / 2;
+
+    const SIDES = 32;
+    const angleStep = (Math.PI * 2) / SIDES;
+    const circle = [];
+
+    for (let i = 0; i < SIDES; i++) {
+      circle.push({
+        x: (x + Math.cos(angleStep * i) * r) * SCALE,
+        y: (y + Math.sin(angleStep * i) * r) * SCALE,
+      });
+    }
+
+    return circle;
+  }
+
+  createPolygon() {
+    const clipper = window.clipper;
+    this.polygon = clipper
+      .clipToPaths({
+        clipType: 'union',
+        subjectFillType: 'positive',
+        subjectInputs: this.circles.map((circle) => {
+          return {
+            data: circle,
+            closed: true,
+          };
+        }),
+      })
+      .map((part) => {
+        return simplify(part, SIMPLIFY_TOLERANCE * SCALE);
+      });
+  }
+
   draw(isLastStep) {
     p5.fill(this.color);
     p5.circle(this.position.x, this.position.y, this.size);
 
+    const x = this.position.x.toFixed(1);
+    const y = this.position.y.toFixed(1);
+    const r = this.size / 2;
+
+    this.circles.push(this.getCircle());
+
+    if (isLastStep) {
+      this.createPolygon();
+    }
+
     if (isLastStep || !this.done) {
-      return `<circle cx="${ this.position.x.toFixed(1) }" cy="${ this.position.y.toFixed(1) }" r="${ this.size / 2 }" fill="${ this.color }" />`
+      return `<circle cx="${x}" cy="${y}" r="${r}" fill="${this.color}" />`;
     }
 
     return '';
@@ -48,18 +109,22 @@ class Particle {
       return;
     }
 
-    const angle = this.angle + (p5.noise(this.seed / 40) * 2 - 1) * Math.PI / 2;
+    const angle =
+      this.angle + ((p5.noise(this.seed / 40) * 2 - 1) * Math.PI) / 2;
     this.seed++;
     this.counter++;
 
     const movement = Math.max(this.size / 40, 1);
 
-    this.position.x += Math.cos(angle) * movement; 
-    this.position.y += Math.sin(angle) * movement; 
+    this.position.x += Math.cos(angle) * movement;
+    this.position.y += Math.sin(angle) * movement;
 
-    this.size += (p5.noise((this.seed + this.counter) / 60) * 1.2) - 0.5;
+    this.size += p5.noise((this.seed + this.counter) / 60) * 1.2 - 0.5;
+
+    if (this.size < 2) {
+      this.size = 2;
+    }
   }
 }
 
 export default Particle;
-
